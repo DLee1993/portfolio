e4409d3fd27cd5c18f8909d27c45af4ce4159038
David Lee <Dai180293@gmail.com>
2024-06-30 20:54:06 +0100

    added header and landing section text content

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
-
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
 function App() {
-    return <main>hello</main>;
+    return (
+        <>
+            <Header />
+            <main>
+                <section id="landing">
+                    <h1>
+                        <span>kickboxer</span>&<span>web developer</span>
+                    </h1>
+                </section>
+            </main>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <header>
+            <p>David lee</p>
+        </header>
+    );
+};
