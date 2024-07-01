e3f3ba6e0d774354fe0897abf58a143ed53032b7
David Lee <Dai180293@gmail.com>
2024-07-01 19:17:51 +0100

    adjusted the height of landing section

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -2,15 +2,15 @@ export const Landing = () => {
     return (
         <section
             id="landing_section"
-            className="relative min-h-screen overflow-x-hidden flex justify-center items-center"
+            className="relative overflow-x-hidden"
         >
-            <section className="w-full flex flex-col justify-between items-start gap-y-14">
+            <section className="w-full min-h-[calc(100vh-var(--headerHeight))] md:min-h-screen flex flex-col justify-evenly items-start gap-y-14">
                 <h1 className="w-full flex justify-center items-center flex-col gap-y-4 font-Roboto uppercase font-light">
-                    <span className="indent-[var(--fs-base)] tracking-[var(--fs-base)] sm:indent-[var(--fs-lg)] sm:tracking-[var(--fs-lg)] md:indent-[var(--fs-xl)] md:tracking-[var(--fs-xl)] lg:indent-[var(--fs-xxxl)] lg:tracking-[var(--fs-xxxxl)] xl:indent-[var(--fs-xxxxl)] xl:tracking-[var(--fs-xxxl)] text-lg sm:text-lg 2xl:text-xl">
+                    <span className="indent-[var(--fs-lg)] tracking-[var(--fs-lg)] sm:indent-[var(--fs-xxl)] sm:tracking-[var(--fs-xxl)] md:indent-[var(--fs-xxxl)] md:tracking-[var(--fs-xxxl)] lg:indent-[var(--fs-xxxl)] lg:tracking-[var(--fs-xxxxl)] xl:indent-[var(--fs-xxxxxl)] xl:tracking-[var(--fs-xxxxxl)] text-lg sm:text-lg 2xl:text-xl">
                         kickboxer
                     </span>
                     <span className="text-base sm:text-md md:text-lg 2xl:text-xl">&</span>
-                    <span className="font-bold leading-none indent-[0.4em] tracking-[0.4em] sm:indent-[var(--fs-md)] sm:tracking-[var(--fs-md)] md:indent-[var(--fs-lg)] md:tracking-[var(--fs-lg)] lg:indent-[var(--fs-lg)] lg:tracking-[var(--fs-lg)] 2xl:indent-[var(--fs-xl)] 2xl:tracking-[var(--fs-xl)] text-lg sm:text-lg md:text-xl lg:text-3xl">
+                    <span className="font-bold leading-none indent-[0.4em] tracking-[0.4em] sm:indent-[var(--fs-md)] sm:tracking-[var(--fs-md)] md:indent-[var(--fs-lg)] md:tracking-[var(--fs-lg)] text-lg sm:text-lg md:text-xl lg:text-3xl">
                         web developer
                     </span>
                 </h1>
@@ -34,11 +34,11 @@ export const Landing = () => {
             </section>
             <section
                 id="projects_snapshot"
-                className="absolute top-0 -right-28 -z-10 flex gap-x-5 opacity-25 h-full"
+                className="absolute top-0 -right-28 -z-10 flex gap-x-5 opacity-10 h-full"
             >
-                <div className="bg-faded-white w-80 h-full rounded-md"></div>
-                <div className="bg-faded-white w-[450px] h-full rounded-md"></div>
-                <div className="bg-faded-white w-80 h-full rounded-md"></div>
+                <div className="bg-red-400 w-80 h-full rounded-md"></div>
+                <div className="bg-blue-400 w-[450px] h-full rounded-md"></div>
+                <div className="bg-green-400 w-80 h-full rounded-md"></div>
             </section>
         </section>
     );
