bf797ba854412590640445d8b2bebf9e4d455d84
David Lee <Dai180293@gmail.com>
2024-07-01 09:50:38 +0100

    adjusted layout for mobile viewport

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
     return (
-        <header>
+        <header className="min-h-[var(--headerHeight)] flex justify-start items-center md:items-end">
             <p className="font-Sacremento text-lg">David lee</p>
         </header>
     );
