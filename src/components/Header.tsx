0346ed799847b4d0e1a6a83f87fbffd79cf0d5b4
David Lee <Dai180293@gmail.com>
2024-07-01 13:27:30 +0100

    centered text on mobile viewport

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
     return (
-        <header className="min-h-[var(--headerHeight)] flex justify-start items-center md:items-end">
+        <header className="min-h-[var(--headerHeight)] flex justify-center md:justify-start items-center md:items-end">
             <p className="font-Sacremento text-lg">David lee</p>
         </header>
     );
